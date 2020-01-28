23ed55cfbb58473cddbad761b40d7724107a3b1d
丁学紧 <accounts_5a503b085f65ba798cb5b115@mail.teambition.com>
2020-01-28 23:20:55 +0800

    feat(router): router
    项目添加路由

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,24 @@
  * @Description: 
  * @Author: dingxuejin
  * @Date: 2020-01-02 13:15:53
- * @LastEditTime : 2020-01-02 17:54:59
+ * @LastEditTime : 2020-01-28 23:01:14
  * @LastEditors  : dingxuejin
  */
 import React from 'react';
+import {Router,Switch,Route} from 'react-router-dom';
+import Home from '@/pages/home/home';
+import {history} from '@/router'
 
 class App extends React.PureComponent{
     render(){
         return (
-            <div>
-                App
-            </div>
+            <Router history={history}>
+                <Switch>
+                    <Route path='/' component={Home}></Route>  
+                </Switch>
+            </Router>
         )
     }
 }
+
 export default App;
---
+++ client/src/pages/home/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Home extends React.PureComponent{
+    render(){
+        return(
+            <div>欢迎页</div>
+        )
+    }
+}
+export default Home;
