cea349d82ac6c130e3b9ac55c982a2d70a9d1d37
丁学紧 <accounts_5a503b085f65ba798cb5b115@mail.teambition.com>
2020-03-20 00:43:01 +0800

    feat(home): 增加home页面

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@
  * @Description:
  * @Author: dingxuejin
  * @Date: 2020-01-02 13:15:53
- * @LastEditTime: 2020-03-19 18:45:54
+ * @LastEditTime: 2020-03-20 00:16:00
  * @LastEditors: dingxuejin
  */
 import React,{Suspense} from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import AppRoute from "@/router/routes";
 import { requireAuthentication } from "@/components/authenticatedComponent/authenticatedComponent";
+import { lazy } from "react";
 import Loading from "@/components/loading/loading";
 
 class App extends React.PureComponent {
